7581eda583932d37cc82d76dd563a3e2ffc5f89f
NickNijskens <nick.nijskens@student.ehb.be>
2021-11-30 12:53:58 +0100

    eerste link met backend

--- web-2-groupproject-frontend-team-cansu-nick-andy/challenges.js
+++ web-2-groupproject-frontend-team-cansu-nick-andy/challenges.js
@@ -1,4 +1,29 @@
-let submit = document.getElementById('submit')
-submit.addEventListener('click', function(){
-let nameChallenge = document.getElementById[]
-})
+window.onload = getData();
+
+async function getData(){
+  let data = await fetch('http://localhost:3000/api/challenges');
+  let json = await data.json();
+
+  let HTMLstring = "";
+  
+  json.forEach((obj) => {
+    HTMLstring += `<div class="block2">
+    <div class="card text-center" style="width: 18rem;">
+        <div class="card-header">
+            Session
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">${obj.name}</h5>
+            <p class="card-text">points: ${obj.points} course: ${obj.course}</p>
+            <a href="#" class="btn btn-primary">Edit</a>
+            <a href="#" class="btn btn-danger">Delete</a>
+        </div>
+
+    </div>
+</div>`
+  })
+
+  document.getElementById("content").innerHTML = HTMLstring;
+}
+
+
